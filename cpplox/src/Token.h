a677022be6354866d257fb030cf83906e00f12a9
Amogh Akshintala <aakshintala@cs.unc.edu>
2020-07-23 23:24:00 -0400

    Add Token header file

---
+++ cpplox/src/Token.h
@@ -0,0 +1,73 @@
+#pragma once
+#include <string>
+
+namespace cpplox {
+
+enum struct TokenType {
+  // Single-character tokens.
+  LEFT_PAREN,
+  RIGHT_PAREN,
+  LEFT_BRACE,
+  RIGHT_BRACE,
+  COMMA,
+  DOT,
+  MINUS,
+  PLUS,
+  SEMICOLON,
+  SLASH,
+  STAR,
+
+  // One or two character tokens.
+  BANG,
+  BANG_EQUAL,
+  EQUAL,
+  EQUAL_EQUAL,
+  GREATER,
+  GREATER_EQUAL,
+  LESS,
+  LESS_EQUAL,
+
+  // Literals.
+  IDENTIFIER,
+  STRING,
+  NUMBER,
+
+  // Keywords.
+  AND,
+  CLASS,
+  ELSE,
+  FALSE,
+  FUN,
+  FOR,
+  IF,
+  NIL,
+  OR,
+  PRINT,
+  RETURN,
+  SUPER,
+  THIS,
+  TRUE,
+  VAR,
+  WHILE,
+
+  EOF
+};
+
+template <typename T>
+class Token {
+public:
+  Token(TokenType p_type, std::string p_lexeme, T p_literal, int p_line)
+      : type(p_type), lexeme(p_lexeme), literal(p_literal), line(p_line);
+
+  std::string toString() {
+    return to_string(type) + " " + lexeme + " " + to_string(literal);
+  }
+
+private:
+  TokenType type;
+  std::string lexeme;
+  T literal;
+  int line;
+};
+
+} // namespace cpplox
